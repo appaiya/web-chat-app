6a9dfb28c7a17e9bc05c3dece98c0bf91029b450
Ram <ramaprasath@interactionone.com>
2021-04-13 11:14:07 +0530

    Chat.js Functinality added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 const publicDirPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirPath));
 
-app.listen(port, ()=> {
-  console.log(`Server is up and Running ${port} !!`);
+io.on("connection" , () =>{
+  console.log("New Socket IO Connection is up and running !!!! @@@ !!!");
 });
 
-
+server.listen(port, ()=> {
+  console.log(`Server is up and Running ${port} !!`);
+});
